f2802d714b02d19fd8493064cea3d96f0f82e022
Konstantin Gribov <grossws@gmail.com>
2015-02-25 16:30:43 +0300

    Add xhost and xtype to tomcat run string

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ "$1" = "tomcat" ] ; then
   chown -R tomcat:tomcat /opt
+  export JAVA_OPTS="-Dxhost=$HOSTNAME -Dxtype=${XTYPE:-TOMCAT} $JAVA_OPTS"
   exec gosu tomcat:tomcat /opt/tomcat/bin/catalina.sh run
 fi
 
