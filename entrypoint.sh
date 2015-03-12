4e95aa26662eceb29714558ff614f21d50cf9b50
Konstantin Gribov <grossws@gmail.com>
2015-03-12 16:46:25 +0300

    Moved JAVA_OPTS to CATALINA_OPTS

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "tomcat" ] ; then
   chown -R tomcat:tomcat /opt
-  export JAVA_OPTS="-Dxhost=$HOSTNAME -Dxtype=${XTYPE:-TOMCAT} $JAVA_OPTS"
+  export CATALINA_OPTS="-Dxhost=$HOSTNAME -Dxtype=${XTYPE:-tomcat} $CATALINA_OPTS"
   exec gosu tomcat:tomcat /opt/tomcat/bin/catalina.sh run
 fi
 
